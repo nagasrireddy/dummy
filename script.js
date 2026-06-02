9d7874bc129eaf1829c326b14e77c7838c5b28d4
Nagasri <2303a51247@sru.edu.in>
2026-06-02 14:27:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const heading = document.getElementById("heading");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+
+    heading.textContent = "Welcome Reddy";
+
+});
